af2f9304ff49fa2a5c715e50c1a046c756956d86
Raph <raph@poir.es>
2017-04-21 17:22:07 +0200

    Add wizz

---
+++ storage_image/__openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Storage Image",
+    "summary": "Store image and resized image in a storage backend",
+    "version": "8.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "www.akretion.com",
+    "author": " Akretion",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "storage_file",
+        "base",
+    ],
+    "data": [
+    #    'views/storage_image_view.xml',
+        'views/partner.xml',
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ]
+}
